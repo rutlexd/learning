94a07209e3f84ec33ed34f9c6159b80f8846dd57
rutlexd <140651300+rutlexd@users.noreply.github.com>
2023-07-27 12:14:42 +0300

    searchs

---
+++ searchs/BinarySearch.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+using namespace std;
+
+int SearchBinary(int arr[], int left, int right, int key) {
+	int mid = 0;
+	while (1) {
+
+		mid = (left + right) / 2;
+
+		if (key < arr[mid])       
+			right = mid - 1;      
+		else if (key > arr[mid])  
+			left = mid + 1;    
+		else                       
+			return mid;           
+
+		if (left > right)          
+			return -1;
+	}
+}
+
+int main() {
+	const int SIZE = 10000;
+	int array[SIZE] = {};
+	int key = 0;
+	int index = 0;
+
+	for (int i = 0; i < SIZE; i++) 
+	{
+
+		array[i] = i + 1;
+		cout << array[i]<< " | ";
+
+	}
+
+
+	cout << "What num u need search?\n";
+	cin >> key;
+
+	index = SearchBinary(array, 0, SIZE, key);
+
+	(index >= 0)
+		?cout << "Yor num index - " << index <<"\n"
+		:cout << "erorrrr";
+
+}
---
+++ searchs/linearysearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int search(int arr[],int key,int SIZE) {
+	for (int i = 0; i < SIZE; i++)
+		if (arr[i] == key)
+			return i;
+	for (int i = 0; i < 12; i++)
+		if (arr[i] != key)
+			return -1;
+
+
+}	
+
+int main() {
+	const int SIZE = 12;
+	int array[SIZE] = {};
+	int index = 0;
+	int key = 0;
+	for (int i = 0; i < SIZE; i++) {
+
+		array[i] = i + 1;
+		cout << array[i] << " | ";
+		
+	}
+
+	cout << "key?\n";
+	cin >> key;
+
+	index = search(array, key, SIZE);
+
+	index>=0
+		? cout << key << " in this list " << index
+		: cout << "nooooooo";
+
+
+
+}
