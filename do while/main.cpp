10c2120a8014b141c55b4fac15a63a6bebe533a1
rutlexd <140651300+rutlexd@users.noreply.github.com>
2023-08-02 10:39:59 +0300

    Add files via upload

---
+++ do while/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+const int size = 30;
+int numbers[size];
+int first = 0;
+int second = 1;
+int sum = 0;
+for(int i = 0;i<size;i++){ // Fibonacci numbers 
+    sum = first + second;
+    numbers[i] = sum;
+    second = sum - second;
+    first = sum;
+}
+for(int i = 0;i<size;i++){
+    cout<<numbers[i]<<" ";
+}
+
+}
