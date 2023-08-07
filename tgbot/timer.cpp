6f8ea61dd48baa26629a0be244bf23b3ca72b9f3
rutlexd <140651300+rutlexd@users.noreply.github.com>
2023-08-07 10:28:38 +0300

    Add files via upload

--- tgbot/timer.cpp
+++ tgbot/timer.cpp
@@ -10,23 +10,22 @@ string devTime()
 
     time_t t = time(nullptr);
     tm* now = localtime(&t);
-
- int a, b, c, d, e, f, x = 0, y = 0;
+ 
+ int dayF, mounthF, yearF, dayS, mounthS, yesrS, x = 0, y = 0;
  int leap, k;
- int sets[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+ int mounths[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-
-a = 18;
-b = 7;
-c = 2023;
-d = now->tm_mday;
-e = now->tm_mon + 1;
-f = now->tm_year + 1900;
+dayF = 18;
+mounthF = 7;
+yearF = 2023;
+dayS = now->tm_mday;
+mounthS = now->tm_mon + 1;
+yesrS = now->tm_year + 1900;
 
 //высчитываем сколько было высокосных лет в заданном интервале
-leap = c;
+leap = yearF;
 k = 0;
-while (leap <= f)
+while (leap <= yesrS)
 {
 
  if (leap % 4 == 0 && leap % 100 != 0 || leap % 400 == 0)
@@ -41,11 +40,11 @@ while (leap <= f)
 
 //высчитываем сколько прошло дней в году до начальной даты
 for(int i =1; i<=12;i++){
-    if(i == b){
-        for(int j = 2;j<b+1;j++){
-            x += sets[j-1];
+    if(i == mounthF){
+        for(int j = 2;j<mounthF+1;j++){
+            x += mounths[j-1];
         }
-        x += a;
+        x += dayF;
     }
 }
 
@@ -53,22 +52,22 @@ for(int i =1; i<=12;i++){
 
 //высчитываем сколько прошло дней в году до конечной даты
 for(int i =1; i<=12;i++){
-    if(i==e){ 
-        for(int j = 2;j<e+1;j++){
-            y += sets[j-1];
+    if(i==mounthS){ 
+        for(int j = 2;j<mounthS+1;j++){
+            y += mounths[j-1];
         }
-    y += d;
+    y += dayS;
     }
 }
-if ((c % 4 == 0 && leap % 100 != 0 || leap % 400 == 0)&&b>2) { k = k - 1;} //если начальная дата высокосный год больше 29 февраля то отнимаем 1 от кол-во высокосных лет
-if ((f % 4 == 0 && leap % 100 != 0 || leap % 400 == 0) && d<=29&&e<3) { k = k - 1;} //если конечная дата до 29 февраля то отнимаем 1 от высокосных лет
+if ((yearF % 4 == 0 && leap % 100 != 0 || leap % 400 == 0)&&mounthF>2) { k = k - 1;} //если начальная дата высокосный год больше 29 февраля то отнимаем 1 от кол-во высокосных лет
+if ((yesrS % 4 == 0 && leap % 100 != 0 || leap % 400 == 0) && dayS<=29&&mounthS<3) { k = k - 1;} //если конечная дата до 29 февраля то отнимаем 1 от высокосных лет
 int resultTime = 0;
-if (c == f)
+if (yearF == yesrS)
 {
  resultTime = (y - x)+k; 
 }
 else
-resultTime = (f - c) * 365 + ((y - x)+k);
+resultTime = (yesrS - yearF) * 365 + ((y - x)+k);
 string days = to_string(resultTime);
 string textStudy1 = "U study c++ ";
 string textStudy2 = " days";
